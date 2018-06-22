80aca49521fe3aeb81dbdc3bf1568abff7950065
nickgkg <nickgkg@gmail.com>
2018-06-22 10:44:48 -0400

    very basic framework

---
+++ Maze/Assets/MazeCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateMaze() {
+	
+	}
+
+	void ShuffleMaze() {
+	
+	}
+
+	void HideMaze() {
+		
+	}
+}
---
+++ Maze/Assets/Root.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root : MonoBehaviour {
+	// use this for global code/timer stuff - nick
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
